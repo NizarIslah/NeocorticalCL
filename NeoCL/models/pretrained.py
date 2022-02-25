1a8b7a12364cb4240ad6aa9fa5fde4198e41f4b8
NizarIslah <nizar.isl17@gmail.com>
2022-02-24 23:04:47 -0500

    add image-text dataset

--- NeoCL/models/pretrained.py
+++ NeoCL/models/pretrained.py
@@ -1,9 +1,13 @@
 import torch
 from torch import nn
 from avalanche.models import IncrementalClassifier
-from torch.nn import Linear, ReLU, MultiheadAttention
+from avalanche.training import ICaRL
+from torch.nn import Linear, ReLU, MultiheadAttention, Sequential
 import clip
-
+from torchvision import datasets
+from collections import defaultdict, deque
+import itertools
+    
 class SSLIcarl(nn.Module):
     def __init__(self, pretrained_net, embedding_size, num_classes):
         super(SSLIcarl, self).__init__()
@@ -15,25 +19,56 @@ class SSLIcarl(nn.Module):
         x = self.feature_extractor(x)  # Already flattened
         x = self.classifier(x)
         return x
+
+
+class ImageNetWiki(datasets.CIFAR100):
+    def __init__(self, root, class_text, transforms=None, text_transforms=None, context_len=77, train=True):
+        split = 'train' if train else 'val'
+        super().__init__(root, train)
+        self.transforms = transforms
+        self.n_classes = 100
+        self.class_text = class_text
+        self.context_len = context_len
+        self.text_transforms = text_transforms
+
+    def __getitem__(self, index):
+        im, label = super().__getitem__(index)
+        text = self.class_text[label]['articles'][0][:self.context_len]
+        text = clip.tokenize(text).squeeze()
+        if self.text_transforms:
+            text = self.text_transforms(text)
+        if self.transforms:
+            im = self.transforms(im)
+        text = tex.repeat(text.unsqueeze()[:,None,None])
+        print(im.shape,text.shape)
+        im_text=torch.cat([im,text,0])
+        return im, label
     
+
 class CLIP_Attention(nn.Module):
-    def __init__(self, pretrained_net, preprocess, num_classes, num_heads, embed_dim=2048):
+    def __init__(self, pretrained_net, preprocess, num_classes, num_heads, embed_dim=2048, text_len=77):
         super(CLIP_Attention, self).__init__()
         self.preprocess = preprocess
-        self.model = pretrained_net
-        self.attention = MultiheadAttention(embed_dim, num_heads)
-        self.classifier = Linear(embed_dim, num_classes)
+        self.text_len=text_len
+        self.feature_extractor = pretrained_net
+        self.classifier = Sequential(
+            MultiheadAttention(embed_dim, num_heads),
+            ReLU(),
+            Linear(embed_dim, num_classes)
+        )
     
-    def forward(self, image, text):
+    def forward(self, x):
+        image, text = x[:, :-self.text_len], x[:, -self.text_len:]
+        print(image.shape, text.shape)
         image = self.preprocess(image)
-        text = clip.tokenize(text)
         with torch.no_grad():
-            img_features = self.model.encode_image(image)
-            text_features = self.model.encode_text(text)
-            print(text_features.shape)
-        atn_out = self.attention(query=img_features, key=text_features, value=img_features)
-        logits = self.classifier(atn_out)
-        return logits
+            img_features = self.feature_extractor.encode_image(image)
+            text_features = self.feature_extractor.encode_text(token)
+        layers = [module for module in self.classifier.modules() if not isinstance(module, nn.Sequential)]
+        out = layers[0](query=img_features, key=text_features, value=img_features)
+        for l, module in enumerate(layers[1:]):
+            out = module(out)
+        return out
 
 class PretrainedIncrementalClassifier(IncrementalClassifier):
     """
